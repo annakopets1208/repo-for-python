47c99ce2ef767c06f1780b5a3f3d99220916ac47
Anna Kopets <akopc2012@gmail.com>
2026-09-03 07:48:57 +0300

    day-1-challenge

---
+++ file.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+# print(2 + 3)
+# print(3 ** 4)
+# print(4 % 13)
+# print(10 // 7)
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('Anna Kopets'))
+print(type([4, 5, 2]))
+print(type({'dfjo': 'dwuirhfi', 'dwuirhfi': '3564'}))
+print(type({3.7, 7.2, 4.7}))
