391145509a3962a88e85e445faaf639bdb3494c0
Anna Kopets <akopc2012@gmail.com>
2026-09-04 12:35:56 +0300

    day-2

---
+++ 30-day-challenge/day-2.py
@@ -0,0 +1,32 @@
+print(help('keywords'))
+
+# Variables in Python
+first_name = 'Anna'
+last_name = 'Kopets'
+country = 'Ukraine'
+city = 'Odesa'
+age = 18
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Anna',
+   'lastname':'Kopets',
+   'country':'Ukraine',
+   'city':'Odesa'
+   }
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+num_int = 10
+print(num_int)
+num_str = str(num_int)
+print(num_str)
